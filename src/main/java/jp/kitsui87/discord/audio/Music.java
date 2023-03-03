1750484fd8b7f33f946df0d937fb072a576db84d
risusan87 <risusan8795@gmail.com>
2023-03-03 10:24:04 -0500

    Music class a little bit organized

--- src/main/java/jp/kitsui87/discord/audio/Music.java
+++ src/main/java/jp/kitsui87/discord/audio/Music.java
@@ -9,6 +9,8 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.security.GeneralSecurityException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
@@ -19,11 +21,14 @@ import javax.sound.sampled.AudioSystem;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
+import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
 
 /**
- * Class represents music.
+ * Class represents music that provides low level functionality to play an audio data.
  * 
  * To obtain the object of this class, use the static method provided in this class.
  * Music object will be in response of keeping track on current location in stream of data of 
@@ -43,15 +48,28 @@ import com.google.api.services.youtube.model.VideoListResponse;
 public class Music {
     
     private static YouTube clientAPI = null;
+    static {
+        try {
+            clientAPI = new YouTube.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), 
+                JacksonFactory.getDefaultInstance(), null).setApplicationName("app").build();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public final long lengthSeconds;
     public final String musicURL;
     public final String title;
 
     public AudioFormat audioFormat = new AudioFormat(48000f, 16, 2, true, false);
-    private InputStream rawStream = null;
     private AudioInputStream audioStream = null;
     private int currentBytesRead = 0;
+    // Flag used to close the download stream
+    private boolean streamTerminate = true;
+    // Flas used to indicate if stream has reached the end
     public boolean hasFinished = false;
 
     protected Music(long length, String title, String url) {
@@ -73,41 +91,81 @@ public class Music {
 
     /**
      * Initiates the download of this music.
-     * 
+     * This method must get called before start calling getAudioData()
      */
-    public void loadMusic() {
+    public void openDownloadStream() {
 
-        ProcessBuilder ffmpegBuilder = new ProcessBuilder(
-            "ffmpeg", "-i", musicURL,
-            "-f", "s16be",
-            "-acodec", "pcm_s16be",
-            "-ar", "48000",
-            "-ac", "2",
-            "pipe:1"
-        );
-        try {
-            this.rawStream = new BufferedInputStream(ffmpegBuilder.start().getInputStream());
-            this.audioStream = new AudioInputStream(this.rawStream, audioFormat, AudioSystem.NOT_SPECIFIED);
-            this.audioStream.mark(Integer.MAX_VALUE);
-            this.hasFinished = false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            this.disposeStream();
+        if (audioStream != null)
             return;
-        }
         
+        Thread asyncThread = new Thread(() -> {
+            ProcessBuilder ffmpegBuilder = new ProcessBuilder(
+                "ffmpeg", "-i", musicURL,
+                "-f", "s16be",
+                "-acodec", "pcm_s16be",
+                "-ar", "48000",
+                "-ac", "2",
+                "pipe:1"
+            );
+            Process ffmpeg;
+            try {
+                ffmpeg = ffmpegBuilder.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            try (
+                InputStream is = new BufferedInputStream(ffmpeg.getInputStream());
+                AudioInputStream audioStream = new AudioInputStream(is, this.audioFormat, AudioSystem.NOT_SPECIFIED);
+            ) {
+                Music.this.audioStream = audioStream;
+                Music.this.streamTerminate = false;
+                Music.this.currentBytesRead = 0;
+                Music.this.hasFinished = false;
+                while (!Music.this.streamTerminate) {
+                    Thread.sleep(10);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                ffmpeg.destroyForcibly();
+                Music.this.audioStream = null;
+            }
+            
+        });
+        asyncThread.start();
+
     }
 
-    public void rewind() {
-        try {
-            this.audioStream.reset();
-            this.hasFinished = false;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Closes the download stream.
+     * By the end of this method process any resources that is nessessary to obtain audio data become void.
+     */
+    public synchronized void closeDownloadStream() {
+       
+        if (this.audioStream == null)
+            return;
+        this.streamTerminate = true;
+        while (this.audioStream != null)
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
     }
 
-    public ByteBuffer getAudioData(int byteLength) {
+    /**
+     * Retrieves the specified size of audio data contained in a byte buffer from the audio stream.
+     * The download stream must first get opened before call of this method.
+     * 
+     * @param byteLength
+     * @return
+     */
+    public synchronized ByteBuffer getAudioData(int byteLength) {
+
+        if (this.audioStream == null)
+            return null;
 
         byte[] b = new byte[byteLength];
         ByteBuffer buffer = ByteBuffer.allocate(byteLength);
@@ -115,6 +173,7 @@ public class Music {
  
         try {
             bytesRead = this.audioStream.read(b);
+            System.out.println(bytesRead + " : " + this.getPlayedTime());
             if (bytesRead == -1) {
                 this.hasFinished = true;
                 return null;
@@ -126,14 +185,9 @@ public class Music {
             return buffer;
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println("An error has been occured while retrieving data from the stream. This most likely to happen when the music has not been loaded.");
             return null;
         }
 
-    }
-
-    public boolean audioClosed() {
-        return this.audioStream == null;
     }
 
     public float getPlayedTime() {
@@ -141,20 +195,6 @@ public class Music {
         int bitrate = this.getAudioBitrate();
         int bytePerSecond = bitrate / 8;
         return (float) this.currentBytesRead / (float) bytePerSecond;
-
-    }
-
-    public void disposeStream() {
-
-        if (this.audioStream == null)
-            return;
-
-        try {
-            this.audioStream.close();
-            this.rawStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
     
@@ -167,27 +207,16 @@ public class Music {
      * To get the result, call Future.get(). This call will block the main process to wait
      * until the retrieval finishes, then returns the Music object.
      * 
-     * @param ytURL
+     * @param ytURL  URL to the single video.
      * @return Future object represents asynchrinous task retrieving the url.
      */
     public static Future<Music> getYouTubeAudio(String ytURL) {
         
-        if (!ytURL.startsWith("https://www.youtube.com/watch?v="))
+        if (!(ytURL.startsWith("https://www.youtube.com") && (ytURL.contains("v="))))
             return null;
-        
+
         if (clientAPI == null) {
-            try {
-                clientAPI = new YouTube.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(), 
-                    JacksonFactory.getDefaultInstance(), null).setApplicationName("app").build();
-            } catch (GeneralSecurityException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (clientAPI == null) {
-                return null;
-            }
+            return null;
         }
 
         long length = -1;
@@ -216,19 +245,78 @@ public class Music {
 
             ProcessBuilder builder = new ProcessBuilder("yt-dlp", "-g", ytURL);
             String result = null;
+            Process ytdlp = null;
+            try {
+                ytdlp = builder.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
             try (
-                InputStreamReader isr = new InputStreamReader(builder.start().getInputStream());
+                InputStreamReader isr = new InputStreamReader(ytdlp.getInputStream());
                 BufferedReader br = new BufferedReader(isr);
             ) {
                 String line;
                 while ((line = br.readLine()) != null) result = line;
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                ytdlp.destroy();
             }
             return new Music(f_length, f_title, result);
 
         });
 
+    }
+
+    /**
+     * Retrieves the youtube link from the url provided.
+     * If the link is direct to a single video, return List is size of 1 and will
+     * contain the ID of the video.
+     * If the link is to a play list, it will return the List of video IDs inside
+     * the
+     * playlist.
+     */
+    public static List<String> getYouTubeLinks(String url) {
+
+        if (clientAPI == null)
+            return null;
+
+        String id;
+        if (url.startsWith("https://www.youtube.com/"))
+            if (url.contains("list=")) {
+                id = url.split("list=")[1];
+                try {
+                    YouTube.PlaylistItems.List playListRequest = clientAPI.playlistItems().list("snippet");
+                    playListRequest.setPlaylistId(id);
+                    playListRequest.setKey("AIzaSyCsHtyayRx1PPB14EVlRkOdYQcTR8lXYc8");
+                    String nextPageToken = "";
+                    List<String> videoIds = new ArrayList<>();
+                    do {
+                        playListRequest.setPageToken(nextPageToken);
+                        PlaylistItemListResponse playlistItemsResponse = playListRequest.execute();
+                        List<PlaylistItem> playlistItems = playlistItemsResponse.getItems();
+                        for (PlaylistItem playlistItem : playlistItems) {
+                            ResourceId resourceId = playlistItem.getSnippet().getResourceId();
+                            if (resourceId.getKind().equals("youtube#video")) {
+                                videoIds.add(resourceId.getVideoId());
+                            }
+                        }
+                        nextPageToken = playlistItemsResponse.getNextPageToken();
+                    } while (nextPageToken != null);
+
+                    return videoIds;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            } else {
+                List<String> l = new ArrayList<>();
+                l.add(url.split("v=")[1]);
+                return l;
+            }
+        else
+            return null;
     }
 
 }
